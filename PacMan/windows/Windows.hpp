f87e2aafbaa33623b51fcd3aa76d4f0559252be6
akhilesh mishra <akmishra@adobe.com>
2020-05-07 14:55:32 +0530

    Adding windows

---
+++ PacMan/windows/Windows.hpp
@@ -0,0 +1,14 @@
+//
+//  Windows.hpp
+//  PacMan
+//
+//  Created by Akhilesh Mishra on 07/05/20.
+//  Copyright © 2020 Akhilesh Mishra. All rights reserved.
+//
+
+#ifndef Windows_hpp
+#define Windows_hpp
+
+#include <stdio.h>
+
+#endif /* Windows_hpp */
